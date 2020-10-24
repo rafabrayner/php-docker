68f76674de6a55cd9a446efbbb80886edbbc916c
Rafael Brayner Miranda de Carvalho <brayner@Rafaels-MacBook-Pro.local>
2020-10-24 17:11:16 -0300

    Atualiza pagina incial

--- www/index.php
+++ www/index.php
@@ -1,13 +1,49 @@
-<!DOCTYPE HTML>
-<html>
+<!DOCTYPE html>
+<html lang="pt-br">
     <head>
-        <title>Exemplo Atualizado</title>
+        <title>Conceitos OO - PHP</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
     </head>
     <body>
+        <div class="jumbotron text-center">
+            <h1>Conceitos OO</h1>
+            <p>Conceitos de programação OO com PHP</p> 
+        </div>
 
-        <?php
-            echo "Olá, eu sou um script PHP!";
-        ?>
-
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Herança</h5>
+                            <p class="card-text">Aplicando os conceitos de Herança em OO à liguagem de programação PHP.</p>
+                            <a href="heranca" class="btn btn-primary">Visitar</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Polimorfismo</h5>
+                            <p class="card-text">Aplicando os conceitos de Polimorfismo em OO à liguagem de programação PHP.</p>
+                            <a href="polimorfismo" class="btn btn-primary">Visitar</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Banco de Dados</h5>
+                            <p class="card-text">Realizando INSERT, SELECT e DELETE no MySQL utilizando PHP PDO.</p>
+                            <a href="php_bd/exemploSelect.php" class="btn btn-primary">Visitar</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
